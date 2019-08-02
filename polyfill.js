8f032b58ccaf0916cf4401a48136dcce451b2b44
ploychanok jaleansook <41948710+toonNvk@users.noreply.github.com>
2019-08-02 10:28:10 +0700

    update polyfill
    library of 'react-app-polyfill/ie10' is not exist because ie 9 is already support for ie 9-11

--- polyfill.js
+++ polyfill.js
@@ -1,2 +1,3 @@
 import 'react-app-polyfill/ie11'; // for ie 11
+//import 'react-app-polyfill/ie9'; // for ie 9-11 support Note: 'react-app-polyfill/ie10' is not exist
 import 'react-app-polyfill/stable';
